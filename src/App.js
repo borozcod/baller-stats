9bb12632109a1a7ae8cf026d58603f9ae0dbc98c
Bryan Orozco <bryanorozcoweb@gmail.com>
2020-02-27 17:56:16 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
         return (
             <div className="baller-stats mh4-ns mh2">
                 <div className="flex items-center justify-center pa3">
-                    <i className="fas fa-basketball-ball mr2 orange f3"></i><span className="f3">Baller Stats</span>
+                    <i className="fas fa-basketball-ball mr2 orange f3"></i><span className="f3">Baller Stats!</span>
                 </div>
                 <Home />
             </div>
