9fcf3aab9ee6ce3a01e9382d851d98d985495c70
Bryan Orozco <bryanorozcoweb@gmail.com>
2021-09-16 15:49:22 -0700

    dev: updated chart

--- src/components/graph/Graph.js
+++ src/components/graph/Graph.js
@@ -1,59 +1,45 @@
-import React, { useContext, useEffect, useRef } from 'react';
-import { Chart, registerables } from 'chart.js';
+import React, { useContext } from 'react';
 import { getColor } from './../../utils/graph-utils';
 import StatsContext from './../../context/stats-context';
 import _ from 'lodash'
-Chart.register(...registerables);
-var chart =  new Chart();
+import { Radar } from 'react-chartjs-2';
+
 let fields = [
     '2pt-made',
-    '2pt-attempted' ,
     '3pt-made',
-    'ft-mades',
-    'ft-attempted',
     'rebounds',
-    'steals',
     'assists',
+    'steals',
     'blocks',
-    'points',
 ]
 
 const Graph = () => {
-    const canvasRef = useRef(null);
 
     const {
         stats
     } = useContext(StatsContext);
 
-    useEffect(() => {
+    const chartDataset = [];
 
-        chart.destroy()
-        const chartData = [];
-
-        _.forEach(stats, (teamMember, i) => {
-            const pickData = _.pick(teamMember, fields);
-            const pickDataValues = _.values(pickData);
-            chartData.push({
-                label: `${teamMember['first']} ${teamMember['last']}`,
-                data: pickDataValues,
-                backgroundColor: getColor(i)
-            });
+    _.forEach(stats, (teamMember, i) => {
+        const pickData = _.pick(teamMember, fields);
+        const pickDataValues = _.values(pickData);
+        chartDataset.push({
+            label: `${teamMember['first']} ${teamMember['last']}`,
+            data: pickDataValues,
+            backgroundColor: getColor(i)
         });
+    });
 
-        chart = new Chart(canvasRef.current, {
-            type: 'radar',
-            data: {
-                labels: fields,
-                datasets: chartData
-
-            }
-        });
-    })
+    const chartData = {
+        labels: fields,
+        datasets: chartDataset
+    }
 
     return (
         <div>
             <div className="mw6 center">
-                <canvas ref={canvasRef} id="graph" height="400"></canvas>
+                <Radar data={chartData}/>
             </div>
         </div>
     );
