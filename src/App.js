ac8ff10be875abae7bf31d61fba61e6efe5a6033
Bryan Orozco <bryanorozcoweb@gmail.com>
2021-09-17 11:04:04 -0700

    dev: added updated graph

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
-import Home from "./containers/Home";
+import React, { useState, useEffect } from 'react';
+import Home from './containers/Home';
+import { getTeams, getLeagueLeaders } from './functions'
 import { StatsContextProvider } from './context/stats-context.js';
 
 const App = () => {
 
     const [stats, setStats] = useState([])
     const [teams, setTeams] = useState([])
+
+    useEffect(() => {
+        const loadData = async () => {
+            const teamReq = await getTeams();
+            const leadersReq = await getLeagueLeaders();
+
+            setTeams(teamReq.data);
+            setStats(leadersReq.data);
+        }
+        loadData();
+    }, []);
 
     const contextData = {
         stats: stats,
--- src/components/graph/Graph.js
+++ src/components/graph/Graph.js
@@ -1,27 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { getColor } from './../../utils/graph-utils';
 import StatsContext from './../../context/stats-context';
+import { getLeagueLeaders } from './../../functions';
 import _ from 'lodash'
 import { Radar } from 'react-chartjs-2';
 
 let fields = [
-    '2pt-made',
-    '3pt-made',
-    'rebounds',
-    'assists',
-    'steals',
-    'blocks',
+    'aggressiveness',
+    'offensive-efficiency',
+    'defensive-rating',
+    'rebounding',
+    'playmaking',
 ]
 
-const Graph = () => {
-
-    const {
-        stats
-    } = useContext(StatsContext);
-
+const formatGraphData = (apiData) => {
     const chartDataset = [];
-
-    _.forEach(stats, (teamMember, i) => {
+    _.forEach(apiData, (teamMember, i) => {
         const pickData = _.pick(teamMember, fields);
         const pickDataValues = _.values(pickData);
         chartDataset.push({
@@ -31,13 +25,63 @@ const Graph = () => {
         });
     });
 
-    const chartData = {
+    const chartDataFormatted = {
         labels: fields,
         datasets: chartDataset
     }
 
+    return chartDataFormatted;
+}
+
+const Graph = () => {
+
+    const {
+        stats,
+        teams
+    } = useContext(StatsContext);
+
+    const [leagueLeaders, setLeagueLeaders] = useState([]);
+    const [graphData, setGraphData] = useState([]);
+    const [teamSelect, setTeamSelect] = useState('all');
+
+    useEffect(() => {
+        const loadData = async () => {
+            const { data } = await getLeagueLeaders();
+            setLeagueLeaders(data);
+            setGraphData(data);
+        }
+        loadData();
+    }, [])
+
+    const handleTeamChange = async (e) => {
+        const teamName = e.target.value;
+        setTeamSelect(teamName);
+
+        if(teamName == 'all') {
+            const { data } = await getLeagueLeaders();
+            setGraphData(data);
+
+            return;
+        }
+
+        const teamData = _.filter(leagueLeaders, ['team-name', teamName]);
+        setGraphData(teamData);
+    }
+
+    const renderTeams = (thisTeam, i) => {
+        return(
+            <option key={`team-${i}`} value={thisTeam.name}>{thisTeam.name}</option>
+        )
+    }
+
+    const chartData = formatGraphData(graphData);
+
     return (
         <div>
+            <select className="mr2" value={teamSelect} onChange={handleTeamChange}>
+                <option value="all">All teams</option>
+                {teams.map(renderTeams)}
+            </select>
             <div className="mw6 center">
                 <Radar data={chartData}/>
             </div>
